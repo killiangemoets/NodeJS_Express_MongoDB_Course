1e7e11600fa5346c90f1014872ae87d4b2168495
Killian Gemoets <kilgem@live.fr>
2022-07-14 01:04:28 +0200

    project

---
+++ 3-project/dev-data/data/import-dev-data.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
+const Tour = require('./../../models/tourModel');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+const options = {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(DB, options).then((con) => {
+  console.log('DB connection successful!');
+});
+
+//READ JSON FILE
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// IMPORT DATA INTO DB
+const importData = async () => {
+  try {
+    // The create method can also accept an array of objects
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit(); //it's kind of an aggressive way to stop an application but not a problem here
+};
+
+//DELETE ALL DATA FROM COLLECTION
+const deleteData = async () => {
+  try {
+    // The create method can also accept an array of objects
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit(); //it's kind of an aggressive way to stop an application but not a problem here
+};
+
+console.log(process.argv);
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
