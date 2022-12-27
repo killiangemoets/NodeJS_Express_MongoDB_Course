6b65519d6e81b0cbc87112d303eb4bd17e3f424a
Killian Gemoets <kilgem@live.fr>
2022-12-27 17:40:06 +0100

    booking

--- 3-project/public/js/login.js
+++ 3-project/public/js/login.js
@@ -6,7 +6,7 @@ export const login = async (email, password) => {
     const res = await axios({
       method: 'POST',
       // url: 'http://127.0.0.1:3000/api/v1/users/login',
-      url: 'http://localhost:3000/api/v1/users/login',
+      url: '/api/v1/users/login',
       data: {
         email,
         password,
@@ -30,7 +30,7 @@ export const logout = async () => {
   try {
     const res = await await axios({
       method: 'GET',
-      url: 'http://localhost:3000/api/v1/users/logout',
+      url: '/api/v1/users/logout',
     });
     if (res.data.status === 'success') location.reload(true); // reload from the server and not from browser cache (to update the navbar)
   } catch (err) {
